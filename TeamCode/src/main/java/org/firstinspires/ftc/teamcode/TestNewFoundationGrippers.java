9914a1577d6f946f98acb0e4e4314d94e2bba246
Theo Marshall <theo@seelye.net>
2020-01-11 14:55:23 -0800

    fixed name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestNewFoundationGrippers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestNewFoundationGrippers.java
@@ -51,8 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
+@TeleOp(name="test foundation grippers", group="Linear Opmode")
 public class TestNewFoundationGrippers extends LinearOpMode {
 
     // Declare OpMode members.
